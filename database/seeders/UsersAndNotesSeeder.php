341c4a39d51f1ea994a6871b832aa998d72b5efe
adwindea <danadwnd@gmail.com>
2020-10-22 14:53:26 +0700

    heroku

--- database/seeders/UsersAndNotesSeeder.php
+++ database/seeders/UsersAndNotesSeeder.php
@@ -19,7 +19,7 @@ class UsersAndNotesSeeder extends Seeder
      */
     public function run()
     {
-        $numberOfUsers = 10;
+        $numberOfUsers = 0;
         $numberOfNotes = 100;
         $usersIds = array();
         $statusIds = array();
@@ -40,7 +40,7 @@ class UsersAndNotesSeeder extends Seeder
             'role_id' => $userRole->id,
             'hierarchy' => 2,
         ]);
-        $guestRole = Role::create(['name' => 'guest']); 
+        $guestRole = Role::create(['name' => 'guest']);
         RoleHierarchy::create([
             'role_id' => $guestRole->id,
             'hierarchy' => 3,
@@ -69,9 +69,9 @@ class UsersAndNotesSeeder extends Seeder
         ]);
         array_push($statusIds, DB::getPdo()->lastInsertId());
         /*  insert users   */
-        $user = User::create([ 
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
+        $user = User::create([
+            'name' => 'Developer',
+            'email' => 'develop@dipos.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -80,33 +80,33 @@ class UsersAndNotesSeeder extends Seeder
         ]);
         $user->assignRole('user');
         $user->assignRole($roleAdmin);
-        for($i = 0; $i<$numberOfUsers; $i++){
-            $user = User::create([
-                'name' => $faker->name(),
-                'email' => $faker->unique()->safeEmail(),
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-                'menuroles' => 'user',
-                'status' => $userStatus[ random_int(0,count($userStatus) - 1) ]
-            ]);
-            $user->assignRole('user');
-            array_push($usersIds, $user->id);
-        }
+        // for($i = 0; $i<$numberOfUsers; $i++){
+        //     $user = User::create([
+        //         'name' => 'User',
+        //         'email' => 'user@email.com',
+        //         'email_verified_at' => now(),
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //         'remember_token' => Str::random(10),
+        //         'menuroles' => 'user',
+        //         'status' => $userStatus[ random_int(0,count($userStatus) - 1) ]
+        //     ]);
+        //     $user->assignRole('user');
+        //     array_push($usersIds, $user->id);
+        // }
         /*  insert notes  */
-        for($i = 0; $i<$numberOfNotes; $i++){
-            $noteType = $faker->word();
-            if(random_int(0,1)){
-                $noteType .= ' ' . $faker->word();
-            }
-            DB::table('notes')->insert([
-                'title'         => $faker->sentence(4,true),
-                'content'       => $faker->paragraph(3,true),
-                'status_id'     => $statusIds[random_int(0,count($statusIds) - 1)],
-                'note_type'     => $noteType,
-                'applies_to_date' => $faker->date(),
-                'users_id'      => $usersIds[random_int(0,$numberOfUsers-1)]
-            ]);
-        }
+        // for($i = 0; $i<$numberOfNotes; $i++){
+        //     $noteType = $faker->word();
+        //     if(random_int(0,1)){
+        //         $noteType .= ' ' . $faker->word();
+        //     }
+        //     DB::table('notes')->insert([
+        //         'title'         => $faker->sentence(4,true),
+        //         'content'       => $faker->paragraph(3,true),
+        //         'status_id'     => $statusIds[random_int(0,count($statusIds) - 1)],
+        //         'note_type'     => $noteType,
+        //         'applies_to_date' => $faker->date(),
+        //         'users_id'      => $usersIds[random_int(0,$numberOfUsers-1)]
+        //     ]);
+        // }
     }
 }
