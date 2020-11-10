c89534634d509a39a463c603142a44de82aa37f2
Ardana Adwindea <danadwnd@gmail.com>
2020-11-10 21:11:21 +0700

    continue order page

--- coreui/src/views/order/Order.vue
+++ coreui/src/views/order/Order.vue
@@ -1,5 +1,6 @@
 <style lang="scss">
     @import "../../assets/scss/product-card.scss";
+    @import "../../assets/scss/print.scss";
 </style>
 
 <template>
@@ -9,20 +10,46 @@
             title="Print Receipt"
             :show.sync="printModal"
             >
-                <div id="printable">
-                    <table>
-                        <tr>
-                            <th>asd</th>
-                            <th>asd</th>
-                            <th>asd</th>
-                        </tr>
-                        <tr>
-                            <td>123</td>
-                            <td>123</td>
-                            <td>123</td>
-                        </tr>
-                    </table>
-                </div>
+                <div class="ticket">
+            <!-- <img src="./logo.png" alt="Logo"> -->
+            <p class="centered">ini logo</p>
+            <p class="centered">RECEIPT EXAMPLE
+                <br>Address line 1
+                <br>Address line 2</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th class="quantity">Q.</th>
+                        <th class="description">Description</th>
+                        <th class="price">$$</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="quantity">1.00</td>
+                        <td class="description">ARDUINO UNO R3</td>
+                        <td class="price">$25.00</td>
+                    </tr>
+                    <tr>
+                        <td class="quantity">2.00</td>
+                        <td class="description">JAVASCRIPT BOOK</td>
+                        <td class="price">$10.00</td>
+                    </tr>
+                    <tr>
+                        <td class="quantity">1.00</td>
+                        <td class="description">STICKER PACK</td>
+                        <td class="price">$10.00</td>
+                    </tr>
+                    <tr>
+                        <td class="quantity"></td>
+                        <td class="description">TOTAL</td>
+                        <td class="price">$55.00</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p class="centered">Thanks for your purchase!
+                <br>parzibyte.me/blog</p>
+        </div>
                 <footer slot="footer">
                     <CButton color="warning" class="text-center" @click="printReceipt()">Print</CButton>
                 </footer>
